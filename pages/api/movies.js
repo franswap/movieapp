eee2c79cb91152ae57fdb103d5203e63e094b627
franswap <fplaniol@outlook.fr>
2024-02-26 16:53:29 +0100

    switch cas post get

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -4,6 +4,14 @@ import clientPromise from "../../lib/mongodb";
 export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db("sample_mflix");
-    const movies = await db.collection("movies").find({}).limit(10).toArray();
-    res.json({ status: 200, data: movies });
+
+    switch (req.method) {
+        case "POST":
+            res.json({ status: 200, data: "POST METHOD" });
+        break;
+        case "GET":
+        const movies = await db.collection("movies").find({}).limit(10).toArray();
+        res.json({ status: 200, data: movies });
+        break;
+        }
 }
